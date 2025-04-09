670bd291ec1307c879de1ceea43f481de2ec341d
wareez92 <wareezsowemimo@gmail.com>
2025-04-09 07:37:25 -0500

    Hero section drafted

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,22 @@
-function Home () {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <div>
+        <h1>Headliner</h1>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Reprehenderit, a!
+        </h2>
+        <div>
+          <img src="" alt="Sese Ntem" />
+          <h6>Sese Ntem</h6>
+          <p>Home Lending Advisor</p>
+        </div>
+
+        <button>Apply Now</button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
